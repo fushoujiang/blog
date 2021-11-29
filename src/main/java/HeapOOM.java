271bc9ffde62ae3ad2eb21e5ad4ab5a61d2ed625
fushoujiang <fushoujiang@daojia-inc.com>
2021-11-29 16:55:27 +0800

    堆内存溢出演示

---
+++ src/main/java/HeapOOM.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args:-Xms20m -Xmx20m -XX:HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/fu/Downloads/heapdump.hprof
+ */
+public class HeapOOM {
+    private static class HeapOomObject {
+        int[] ints = new int[1024 * 1024];
+    }
+
+    public static void main(String[] args) {
+        List<HeapOomObject> list = new ArrayList<HeapOomObject>();
+        while (true) {
+            list.add(new HeapOomObject());
+        }
+    }
+}
